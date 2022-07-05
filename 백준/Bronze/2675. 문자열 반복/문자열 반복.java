738ca0b3ed7822c31e2ce51bbe5f7ab1e816fa48
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2022-07-05 17:30:52 +0900

    Update 문자열 반복.java

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int T = in.nextInt();
-		for(int i = 0; i < T; i++) {
-	
-			int R = in.nextInt();
-			String S = in.next();	// nextLine() 을 쓰면 error!
-			
-			for(int j = 0; j < S.length(); j++) {           
-				for(int k = 0; k < R; k++) {	// R 만큼 반복 출력
-					System.out.print(S.charAt(j));
-				}
-			}
-            
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 테스트 케이스 개수
+        
+        int[] numArr = new int[n]; // 반복 횟수 배열
+        String[] wordArr = new String[n]; // 반복 단어 배열
+        
+        // 횟수&단어 입력 받기
+        for(int i=0; i<n; i++) {
+            numArr[i] = sc.nextInt(); 
+            wordArr[i] = sc.next(); 
+        }
+        
+        String[] answer = new String[n];
+        for (int k = 0; k < answer.length; k++) {
+            answer[k] = "";
+        }
+
+        for(int i=0; i<answer.length; i++) { 
+            for(int k=0; k<wordArr[i].length(); k++) {
+                for(int j=0; j<numArr[i]; j++) {
+                    answer[i] += wordArr[i].charAt(k);
+                }
+            }
+        }
+
+        for (String s : answer) {
+            System.out.println(s);
+        }
+    }
 }
