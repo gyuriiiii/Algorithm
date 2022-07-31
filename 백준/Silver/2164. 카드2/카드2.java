9ff0d6eb8177f4cf451fda8529471a742cb93714
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2022-07-31 19:26:53 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 53484 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -7,6 +7,7 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(bf.readLine());
         Queue<Integer> queue = new LinkedList<>();
@@ -15,11 +16,12 @@ public class Main {
             queue.add(i);
         }
 
-        while(queue.size()>1) {
+        while(queue.size()>1) { // 큐가 빌 때 까지
             queue.poll(); // 제일 위의 카드 버리기
             int number = queue.poll(); // 그 다음 위의 카드 옮기기
             queue.offer(number);
         }
-        System.out.println(queue.poll());
+        sb.append(queue.poll());
+        System.out.println(sb);
     }
 }
