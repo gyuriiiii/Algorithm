a4346c750e0b4fca0fbf7616c59f10c851efd9f9
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2022-07-31 17:53:38 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 268 ms, Memory: 33736 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,49 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);    
+        Scanner sc = new Scanner(System.in);  
+        StringBuilder sb = new StringBuilder();  
         
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        int[] arr = new int[n+1];
+        Queue<Integer> queue = new LinkedList<>();
         for(int i=1; i<=n; i++) {
-            arr[i] = i;
+            queue.add(i);
         }
 
-        int idx = 0;
-        int i=1;
-        System.out.print("<");
-        while(true) {
-            if(idx==k*n) {
-                break;
+        sb.append("<");
+        while(!queue.isEmpty()) { // 큐가 빌 때 까지
+            for(int i=0; i<k-1; i++) {
+                int num = queue.poll(); // 큐의 첫번째 값
+                queue.offer(num); // 뒤로 보내기
             }
-            if(arr[i] == 0) {
-                if(i==n) {
-                    i=1;
-                }
-                else {
-                    i++;    
-                }
-                continue;
+            if(queue.size()==1) {
+                sb.append(queue.poll()).append(">");                
             }
             else {
-                idx++;
-                if(idx%k==0) {
-                    arr[i] = 0;
-                    if(idx==k*n) {
-                        System.out.print(i + ">");
-                        break;
-                    }
-                    System.out.print(i + ", ");
-                }
+                sb.append(queue.poll()).append(", ");
             }
-            if(i==n) {
-                i=1;
-                continue;
-            }
-            i++;
-         }
+        }
+        System.out.println(sb);
     }
 }
