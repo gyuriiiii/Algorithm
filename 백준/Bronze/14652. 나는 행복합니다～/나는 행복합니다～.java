ea77fd4c3c05c95a8c4dc3462e9f33d7d97aa561
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2022-08-12 14:55:04 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 828 ms, Memory: 18600 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(k-- == 0) {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+    }
+}
