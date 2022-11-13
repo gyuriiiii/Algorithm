a01e53f4c644109fdc6d7ddb85adcaa13e75ac02
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2022-11-13 11:08:41 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 11.08 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length() == 0) return answer;
+        
+        s = s.toLowerCase();
+        answer += Character.toUpperCase(s.charAt(0));
+        
+        for (int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                answer += " ";
+            }
+            else if(s.charAt(i-1) == ' ') {
+                answer += Character.toUpperCase(s.charAt(i));
+            }
+            else {
+                answer += s.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
