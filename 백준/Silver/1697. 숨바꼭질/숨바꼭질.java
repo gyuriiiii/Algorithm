d613c4b9e035fbbd6904e5e646042641ba90c8b6
hs-1871022-gyuriKim <89003891+gyuriiiii@users.noreply.github.com>
2022-11-21 17:46:53 +0900

    [Silver I] Title: 숨바꼭질, Time: 248 ms, Memory: 21932 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,50 +3,45 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
+    static int N;
+    static int K;
     static int[] check;
-    static int tmp;
-
+    static int q;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); // 수빈이 위치
-        int K = sc.nextInt(); // 동생 위치
+        N = sc.nextInt(); // 수빈이 위치
+        K = sc.nextInt(); // 동생 위치
+
         check = new int[100001]; // 방문 여부 배열
-
-        if (N == K) {
-            System.out.println("0");
-        }
-        else {
-            System.out.println(bfs(N, K));
-        }
+        System.out.println(bfs(check, N));
     }
+    private static int bfs(int[] check, int N) {
+        Queue<Integer> queue = new LinkedList<>(); 
 
-    static int bfs(int N, int K) {
-        Queue<Integer> queue = new LinkedList<>();
+        check[N] = 1; // 방문 여부 표시
+        queue.add(N); // 큐에 추가
 
-        check[N] = 1; 
-        queue.add(N);
+        while(!queue.isEmpty()) { // 큐 빌 때 까지
+            q = queue.poll();
 
-        while (!queue.isEmpty()) {
-            tmp = queue.poll();
-
-            if(tmp == K) {
+            if(q == K) {
                 break;
             }
 
-            if (tmp-1 >= 0 && tmp-1 < 100001 && check[tmp - 1] == 0) {
-                check[tmp - 1] = check[tmp] + 1;
-                queue.add(tmp-1);
+            if(q-1 >= 0 && q-1 < 100001 && check[q-1] == 0) {
+                check[q-1] = check[q] + 1;
+                queue.add(q-1);
             }
-            if (tmp+1 >= 0 && tmp+1 < 100001 && check[tmp + 1] == 0) {
-                check[tmp + 1] = check[tmp] + 1;
-                queue.add(tmp+1);
+            if(q+1 >= 0 && q+1 < 100001 && check[q+1] == 0) {
+                check[q+1] = check[q] + 1;
+                queue.add(q+1);
             }
-            if (tmp*2 >= 0 && tmp*2 < 100001 && check[tmp * 2] == 0) {
-                check[tmp * 2] = check[tmp] + 1;
-                queue.add(tmp*2);
+            if(q*2 >= 0 && q*2 < 100001 && check[q*2] == 0) {
+                check[q*2] = check[q] + 1;
+                queue.add(q*2);
             }
         }
-        return check[tmp]-1;
+        return check[q]-1;
     }
 }
