c30726301c0b8235d2985fede1f6e05e7a60abe8
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-01-09 18:13:18 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 628 ms, Memory: 295192 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,14 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         Queue<Integer> queue = new LinkedList<>();
 
@@ -17,16 +21,15 @@ public class Main {
         }
 
         ArrayList<Integer> list = new ArrayList<>();
-        
+
         int idx = 1;
         while (!queue.isEmpty()) {
             for (int i = 0; i < queue.size(); i++) {
-                if(idx % K == 0) {
+                if (idx % K == 0) {
                     int q = queue.peek();
                     list.add(q);
                     queue.poll();
-                }
-                else {
+                } else {
                     int q = queue.peek();
                     queue.offer(q);
                     queue.poll();
@@ -36,9 +39,9 @@ public class Main {
         }
 
         StringBuilder sb = new StringBuilder("<");
-        for(int i=0; i<list.size(); i++) {
-            if(i==list.size()-1) {
-                sb.append(list.get(i) +">");
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                sb.append(list.get(i) + ">");
                 continue;
             }
             sb.append(list.get(i) + ", ");
