5fd72eb88b9ea56eff7480074c968581a108bf85
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-01-11 16:52:42 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.95 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,18 +1,13 @@
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] A, int[] B) {
+    static public int solution(int[] A, int[] B) {
         Arrays.sort(A);
         Arrays.sort(B);
 
-        int[] arrB = new int[B.length];
-        for (int i = B.length - 1; i >= 0; i--) {
-            arrB[B.length-i-1] = B[i];
-        }
-
         int sum = 0;
         for (int i = 0; i < A.length; i++) {
-            sum += (A[i] * arrB[i]);
+            sum += (A[i] * B[A.length - i - 1]);
         }
         System.out.println(sum);
         return sum;
