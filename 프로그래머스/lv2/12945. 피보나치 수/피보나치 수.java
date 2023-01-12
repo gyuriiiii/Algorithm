f34fc511bd4368032367337301bd7506a8bc54b9
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-01-12 17:22:07 +0900

    [level 2] Title: 피보나치 수, Time: 315.13 ms, Memory: 577 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+class Solution {
+    static public BigInteger solution(int n) {
+        BigInteger[] arr = new BigInteger[100001];
+
+        arr[0] = BigInteger.valueOf(0);
+        arr[1] = BigInteger.valueOf(1);
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 2].add(arr[i - 1]);
+        }
+        return arr[n].remainder(BigInteger.valueOf(1234567));
+    }
+}
