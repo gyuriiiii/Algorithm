7d1066aa933b66a53340a3bf5dad90ab7775c0a6
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-01-18 19:26:55 +0900

    [Silver III] Title: 예산, Time: 428 ms, Memory: 30792 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static long answer; // 배정된 예산 중 가장 큰 값
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // 지방의 수
+        
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        long total = sc.nextLong();
+
+        long low = 0;
+        long high = arr[N-1];
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long sum = 0;
+            
+            for(int i=0; i<N; i++) {
+                if (arr[i] < mid) { // 상한액보다 작은 경우
+                    sum += arr[i]; // 요청금액 그대로 주기
+                }
+                else { 
+                    sum += mid; // 상한액 주기
+                }
+            }
+
+            if(sum > total) { // 초과하는 경우 (상한액 내리기)
+                high = mid - 1;
+            }
+            else { // 상한액 올리기
+                low = mid + 1;
+                answer = Math.max(answer, mid);
+            }
+        }
+        System.out.println(answer);
+    }
+}
