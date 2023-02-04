ef5ba114635bacde1675c58ae4f7f71310d21ce1
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-02-04 17:38:21 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -3,14 +3,9 @@ class Solution {
         int cnt = 0;
 
         for (int i = 0; i < number.length; i++) {
-            int num1 = number[i];
             for (int j = i + 1; j < number.length; j++) {
-                int num2 = number[j];
                 for (int k = j + 1; k < number.length; k++) {
-                    int num3 = number[k];
-
-                    int sum = num1 + num2 + num3;
-                    if(sum == 0) cnt++;
+                    if(number[i] + number[j] + number[k] == 0) cnt++;
                 }
             }
         }
