62c1fd219e68134c9159553e377c95c8a20b2c94
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-02-12 15:41:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1488 ms, Memory: 179172 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,32 +1,41 @@
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
+    private static class Point implements Comparable<Point> {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point p) {
+            if (this.x == p.x) {
+                return Integer.compare(this.y, p.y);
+            } 
+            else {
+                return Integer.compare(this.x, p.x);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[][] arr = new int[n][2];
+        int N = sc.nextInt();
+        Point[] points = new Point[N];
 
-        sc.nextLine();
-        for(int i=0; i<n; i++) {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            points[i] = new Point(sc.nextInt(), sc.nextInt());
         }
+        Arrays.sort(points);
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] arr1, int[] arr2) {
-                if(arr1[0] == arr2[0]) {
-                    return arr1[1]-arr2[1];
-                } 
-                return arr1[0]-arr2[0];
-            }
-        });
-        
-        for (int[] i : arr) {
-            System.out.println(i[0] + " " + i[1]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(points[i].x).append(" ").append(points[i].y).append("\n");
         }
+        System.out.println(sb);
     }
 }
