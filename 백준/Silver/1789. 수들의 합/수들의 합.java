b3755d2de8e5b0410af05dcc1bc2914608df2cda
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-02-18 19:44:58 +0900

    [Silver V] Title: 수들의 합, Time: 212 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long S = sc.nextLong();
+        long sum = 0;
+        long count = 0 ;
+        for (int i = 1; ; i++) {
+            if(sum > S) {
+                break;
+            }
+            sum += i;
+            count++;
+        }
+        System.out.println(count-1);
+    }
+}
