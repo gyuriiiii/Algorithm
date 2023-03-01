e7d7ddaccefadc995ecd0c6a3f6df32a72f6904c
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-03-01 13:59:01 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.36 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,39 +1,42 @@
-import java.util.HashMap;
-
 public class Solution {
     public String solution(int[] numbers, String hand) {
         StringBuilder sb = new StringBuilder();
 
-        HashMap<Integer, int[]> map = new HashMap<>();
-        int idx = 1;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                map.put(idx++, new int[] { i, j }); // 키패드 숫자와 x, y 좌표 저장
-            }
-        }
-        map.put(0, new int[] { 3, 1 });
+        int[][] keypad = {
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}  //9
+        };
 
-        int posL[] = new int[] { 3, 0 }; // 현재 왼손의 x, y위치 저장
-        int posR[] = new int[] { 3, 2 }; // 현재 오른손의 x, y위치 저장
+        // 초기 왼손, 오른손 위치값 설정 (x, y 좌표)
+        int posL[] = new int[] { 3, 0 }; 
+        int posR[] = new int[] { 3, 2 };
 
         for (int i = 0; i < numbers.length; i++) {
             int n = numbers[i]; // 눌러야 할 숫자
 
             if (n == 1 || n == 4 || n == 7) { // 왼손 엄지손가락 사용
                 sb.append("L");
-                posL[0] = map.get(n)[0];
-                posL[1] = map.get(n)[1];
+                posL[0] = keypad[n][0];
+                posL[1] = keypad[n][1];
             }
 
             else if (n == 3 || n == 6 || n == 9) { // 오른손 엄지손가락 사용
                 sb.append("R");
-                posR[0] = map.get(n)[0];
-                posR[1] = map.get(n)[1];
+                posR[0] = keypad[n][0];
+                posR[1] = keypad[n][1];
             }
 
-            else { // 그 외 - 더 가까운 손가락으로 누르기
-                int x = map.get(n)[0]; // 눌러야 할 숫자의 x 좌표
-                int y = map.get(n)[1]; // 눌러야 할 숫자의 y 좌표
+            else { // 그 외 = 더 가까운 손가락으로 누르기
+                int x = keypad[n][0]; // 눌러야 할 숫자의 x 좌표
+                int y = keypad[n][1]; // 눌러야 할 숫자의 y 좌표
 
                 // 왼손과 오른손 중 어떤 손이 더 가까운 지 체크
                 if ((Math.abs(x - posL[0]) + Math.abs(y - posL[1])) < (Math.abs(x - posR[0]) + Math.abs(y - posR[1]))) {
