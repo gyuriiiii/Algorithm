319831cd3a88bcc875b0777f4b17d1f8a38844ae
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-03-03 17:49:56 +0900

    [level 1] Title: 체육복, Time: 0.44 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,30 +1,29 @@
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
+       static public int solution(int n, int[] lost, int[] reserve) {
         int cnt = n - lost.length;
-        
+
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
-        // 여벌 체육복 가져온 학생이 도난 당한 경우
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
                 if (lost[i] == reserve[j]) {
                     cnt++;
+                    reserve[j] = -1; // 여분 있었지만 도난 당한 학생
                     lost[i] = -1;
-                    reserve[j] = -1;
                     break;
                 }
             }
         }
 
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] + 1 == reserve[j] || lost[i] - 1 == reserve[j]) { // 앞뒤 학생
-                    lost[i] = -1;
-                    reserve[j] = -1;
+        for (int j = 0; j < lost.length; j++) {
+            for (int i = 0; i < reserve.length; i++) {
+                if (reserve[i] == lost[j] - 1 || reserve[i] == lost[j] + 1) {
                     cnt++;
+                    reserve[i] = -1;
+                    break;
                 }
             }
         }
