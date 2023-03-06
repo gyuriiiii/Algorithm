a2a24860574fbce412ed2613bf615b1fb2ddb722
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-03-06 18:38:25 +0900

    [level 1] Title: 최소직사각형, Time: 1.65 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,28 +1,14 @@
-import java.util.Arrays;
-import java.util.Collections;
-
-public class Solution {
+class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
+        int height = 0;
+        int length = 0;
 
-        int[] len1 = new int[sizes.length];
-        int[] len2 = new int[sizes.length];
-        for (int i = 0; i < sizes.length; i++) {
-            len1[i] = sizes[i][0];
-            len2[i] = sizes[i][1];
+        for (int[] size : sizes) {
+            height = Math.max(height, Math.max(size[0], size[1]));
+            length = Math.max(length, Math.min(size[0], size[1]));
         }
 
-        int[] result1 = new int[sizes.length];
-        int[] result2 = new int[sizes.length];
-        for (int i = 0; i < sizes.length; i++) {
-            result1[i] = Math.min(len1[i], len2[i]);
-            result2[i] = Math.max(len1[i], len2[i]);
-        }
-        Arrays.sort(result1);
-        Arrays.sort(result2);
-
-        answer = result1[result1.length - 1] * result2[result2.length - 1];
-
+        int answer = height * length;
         return answer;
     }
 }
