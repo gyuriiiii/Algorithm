e8efcb0a24836f1dbb60d1b8c84891535ee8cb70
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-04-01 18:13:03 +0900

    [Silver III] Title: N과 M (1), Time: 436 ms, Memory: 38464 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    static int N;
-    static int M;
-
+    static int N, M;
+    static boolean[] used;
     static int[] arr;
-    static boolean[] check; // 방문 여부
+
+    static StringBuilder sb;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,27 +13,29 @@ public class Main {
         N = sc.nextInt();
         M = sc.nextInt();
 
+        used = new boolean[N + 1];
         arr = new int[M];
-        check = new boolean[N];
-        dfs(0);
+        sb = new StringBuilder();
+
+        backtracking(0);
+        System.out.println(sb);
     }
 
-    private static void dfs(int depth) {
+    private static void backtracking(int depth) {
         if (depth == M) {
-            for (int i : arr) {
-                System.out.print(i + " ");
+            for (int a : arr) {
+                sb.append(a + " ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            if (!check[i]) { // 방문 X
-                check[i] = true;
-                arr[depth] = i + 1;
-                dfs(depth+1);
-
-                check[i] = false;
+        for (int i = 1; i <= N; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                arr[depth] = i;
+                backtracking(depth + 1);
+                used[i] = false;
             }
         }
     }
