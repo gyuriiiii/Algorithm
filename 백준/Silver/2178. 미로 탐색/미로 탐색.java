787aeca8b0f99f3d55b9302064cd2a71b9aa63d7
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-04-05 12:07:35 +0900

    [Silver I] Title: 미로 탐색, Time: 248 ms, Memory: 18884 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,63 +1,72 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Main {
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[][] result;
 
-    static int N;
-    static int M;
-    static boolean[][] check;
-    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
         M = sc.nextInt();
-
-        arr = new int[N][M];
-        check = new boolean[N][M];
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        result = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             String s = sc.next();
-            for (int j = 0; j < s.length(); j++) {
-                arr[i][j] = s.charAt(j) - '0';
+
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j) - '0';
             }
         }
 
+        result[0][0] = 1;
         bfs(0, 0);
-        System.out.println(arr[N-1][M-1]);
+        System.out.println(result[N - 1][M - 1]);
     }
 
-    private static void bfs(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] { i, j });
-        check[i][j] = true;
+    private static void bfs(int x, int y) {
+        ArrayDeque<int[]> dq = new ArrayDeque<>();
 
-        while (!queue.isEmpty()) {
-            int cur[] = queue.poll();
+        dq.add(new int[]{x, y});
+        visited[x][y] = true;
 
-            int curX = cur[0];
-            int curY = cur[1];
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            int curx = cur[0];
+            int cury = cur[1];
 
-            for (int k = 0; k < 4; k++) {
-                int nextX = curX + dx[k];
-                int nextY = curY + dy[k];
+            if (curx == N - 1 && cury == M - 1) {
+                return;
+            }
 
-                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
+            for (int i = 0; i < 4; i++) {
+                int nextx = curx + dx[i];
+                int nexty = cury + dy[i];
+
+                // map 벗어남
+                if (nextx < 0 || nexty < 0 || nextx >= N || nexty >= M) {
                     continue;
                 }
 
-                // 이동할 수 없거나 이미 방문한 칸
-                if (arr[nextX][nextY] == 0 || check[nextX][nextY]) {
+                // 이동할 수 없는 칸
+                if (map[nextx][nexty] == 0) {
                     continue;
                 }
 
-                queue.add(new int[] { nextX, nextY });
-                check[nextX][nextY] = true;
-                arr[nextX][nextY] = arr[curX][curY] + 1;
+                if (!visited[nextx][nexty]) {
+                    dq.add(new int[]{nextx, nexty});
+                    visited[nextx][nexty] = true;
+
+                    result[nextx][nexty] = result[curx][cury] + 1;
+                }
             }
         }
     }
