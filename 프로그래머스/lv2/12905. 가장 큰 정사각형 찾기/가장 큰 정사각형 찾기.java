84673b4056e1484d201ae6cebfb075e2b9dde899
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-04-05 18:02:37 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 15.65 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (board[i][j] > 0) {
+                    board[i][j] += Math.min(Math.min(board[i - 1][j], board[i - 1][j - 1]), board[i][j - 1]);
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                max = Math.max(max, board[i][j]);
+            }
+        }
+        return max * max;
+    }
+}
