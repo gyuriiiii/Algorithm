00508af128af7055ae05f99ca7d929af81498027
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-04-18 16:05:46 +0900

    [Silver II] Title: 사탕 게임, Time: 296 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -3,20 +3,13 @@ import java.util.Scanner;
 public class Main {
     static int N;
     static char[][] map;
-    static boolean[][][][] changed;
-
-    static int[] dx = new int[]{-1, 1, 0, 0};
-    static int[] dy = new int[]{0, 0, -1, 1};
-
     static int answer = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-
         map = new char[N][N];
-        changed = new boolean[N][N][N][N];
 
         for (int i = 0; i < N; i++) {
             String s = sc.next();
@@ -27,37 +20,26 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
+                if (j + 1 < N) {
+                    swap(i, j, i, j + 1);
+                    check_arr();
+                    swap(i, j, i, j + 1);
+                }
 
-                for (int k = 0; k < 4; k++) {
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
-
-                    if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-
-                    if (map[i][j] != map[nx][ny]) {
-                        if (!changed[i][j][nx][ny] && !changed[nx][ny][i][j]) {
-                            change(new int[]{i, j}, new int[]{nx, ny});
-                            changed[i][j][nx][ny] = changed[nx][ny][i][j] = true;
-                        }
-                    }
+                if (i + 1 < N) {
+                    swap(i, j, i + 1, j);
+                    check_arr();
+                    swap(i, j, i + 1, j);
                 }
             }
         }
         System.out.println(answer);
     }
 
-    private static void change(int[] c1, int[] c2) {
-        // 인접한 칸 교환
-        char tmp = map[c1[0]][c1[1]];
-        map[c1[0]][c1[1]] = map[c2[0]][c2[1]];
-        map[c2[0]][c2[1]] = tmp;
-
-        check_arr();
-
-        // 교환했던 칸 되돌리기
-        char tmp2 = map[c1[0]][c1[1]];
-        map[c1[0]][c1[1]] = map[c2[0]][c2[1]];
-        map[c2[0]][c2[1]] = tmp2;
+    private static void swap(int i, int j, int i2, int j2) {
+        char tmp = map[i][j];
+        map[i][j] = map[i2][j2];
+        map[i2][j2] = tmp;
     }
 
     private static void check_arr() {
