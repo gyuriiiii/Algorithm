e392380c98d7a25549892c2e63868739424ce657
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-04-29 02:10:16 +0900

    [Silver I] Title: 경로 찾기, Time: 660 ms, Memory: 30568 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,56 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int N;
-    static int[][] map;
-    static int[][] result;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt();
-        map = new int[N][N];
-        result = new int[N][N];
+        int N = sc.nextInt();
+        int[][] map = new int[N][N];
 
-        HashMap<Integer, ArrayList<Integer>> route = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            route.put(i, new ArrayList<>());
-
             for (int j = 0; j < N; j++) {
                 map[i][j] = sc.nextInt();
-
-                if (map[i][j] == 1) {
-                    ArrayList<Integer> list = route.get(i);
-                    list.add(j);
-                    route.put(i, list);
-                }
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            ArrayDeque<Integer> dq = new ArrayDeque<>();
-            boolean[] visit = new boolean[N];
-
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1) {
-                    result[i][j] = 1;
-
-                    if (!visit[j]) {
-                        visit[j] = true;
-                        dq.add(j);
-                    }
-
-                    while (!dq.isEmpty()) {
-                        int p = dq.poll();
-
-                        ArrayList<Integer> list = route.get(p);
-                        for (int k = 0; k < list.size(); k++) {
-                            result[i][list.get(k)] = 1;
-                            if (!visit[list.get(k)]) {
-                                visit[list.get(k)] = true;
-                                dq.add(list.get(k));
-                            }
-                        }
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][k] == 1 && map[k][j] == 1) {
+                        map[i][j] = 1;
                     }
                 }
             }
@@ -58,7 +25,7 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                System.out.print(result[i][j] + " ");
+                System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
