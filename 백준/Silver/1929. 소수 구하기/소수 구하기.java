21e10dad098a587e994cd6ee2216ab2c43d8bc08
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-06-06 22:44:03 +0900

    [Silver III] Title: 소수 구하기, Time: 340 ms, Memory: 24888 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,30 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-        int[] arr = new int[n+1];
-        for(int i=m; i<=n; i++) {
-            arr[1] = 0;
-            arr[i] = i;
-        }
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
+        boolean[] isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
 
-        for(int i=2; i<=n; i++) {
-            for(int j=2*i; j<=n; j+=i) {
-                if(j<=n) {
-                    arr[j] = 0;
-                }
+        int sqrt = (int) Math.sqrt(N);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i <= sqrt; i++) {
+            if (!isPrime[i])
+                continue;
+
+            for (int j = i * i; j <= N; j += i) {
+                isPrime[j] = false;
             }
         }
 
-        for(int i=m; i<=n; i++) {
-            if(arr[i] != 0) {
-                System.out.println(arr[i]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                sb.append(i + "\n");
             }
         }
+        System.out.println(sb);
     }
 }
