b6969c1b2061effee9d4d6bac5e747a87f42c5f6
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-06-18 18:31:02 +0900

    [Gold IV] Title: 최단경로, Time: 1896 ms, Memory: 326252 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
     static final int INF = 987654321;
-    static boolean[] visited;
     static int[] dis;
     static ArrayList<Node>[] adjList;
 
@@ -15,56 +15,46 @@ public class Main {
         int E = sc.nextInt();
         int K = sc.nextInt();
 
-        visited = new boolean[V + 1];
         dis = new int[V + 1];
-        adjList = new ArrayList[V + 1];
+        Arrays.fill(dis, INF);
 
-        for (int i = 1; i <= V; i++) {
+        adjList = new ArrayList[V + 1];
+        for (int i = 0; i < V + 1; i++) {
             adjList[i] = new ArrayList<>();
-            dis[i] = INF;
         }
 
         for (int i = 0; i < E; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
             int w = sc.nextInt();
-
             adjList[u].add(new Node(v, w));
         }
 
         dijkstra(K);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <=V; i++) {
+        for (int i = 1; i < dis.length; i++) {
             if (dis[i] == INF) {
-                sb.append("INF\n");
+                sb.append("INF").append("\n");
+                continue;
             }
-            else {
-                sb.append(dis[i] + "\n");
-            }
+            sb.append(dis[i]).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static void dijkstra(int k) {
+    private static void dijkstra(int start) {
         PriorityQueue<Node> pq = new PriorityQueue<>(((o1, o2) -> Integer.compare(o1.cost, o2.cost)));
-
-        dis[k] = 0;
-        pq.add(new Node(k, 0));
+        pq.add(new Node(start, 0));
+        dis[start] = 0;
 
         while (!pq.isEmpty()) {
             Node now = pq.poll();
 
-            if (visited[now.end]) {
-                continue;
-            }
-
-            visited[now.end] = true;
-
             for (Node next : adjList[now.end]) {
-                if (dis[next.end] > dis[now.end] + next.cost) {
-                    dis[next.end] = dis[now.end] + next.cost; // 최단 거리 갱신
-                    pq.add(new Node(next.end, dis[next.end])); // 우선순위 큐에 추가
+                if (dis[next.end] > now.cost + next.cost) {
+                    dis[next.end] = now.cost + next.cost;
+                    pq.add(new Node(next.end, dis[next.end]));
                 }
             }
         }
