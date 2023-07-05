7ec220c26864bd06aa8bf2187ed9c7a7623b7b34
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-07-05 16:37:23 +0900

    [unrated] Title: 리코쳇 로봇, Time: 0.90 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -24,65 +24,36 @@ public class Solution {
                 else if (board[i].charAt(j) == 'G') {
                     end = new Node(i, j);
                 }
-
-                cnt[i][j] = Integer.MAX_VALUE;
             }
         }
 
-        bfs(start, board);
-
-        if (cnt[end.x][end.y] == Integer.MAX_VALUE) {
-            return -1;
-        }
-        else {
-            return cnt[end.x][end.y];
-        }
+        return bfs(start, board);
     }
 
-    private static void bfs(Node start, String[] board) {
+    private static int bfs(Node start, String[] board) {
         ArrayDeque<Node> dq = new ArrayDeque<>();
         dq.add(start);
-        cnt[start.x][start.y] = 0;
         visited[start.x][start.y] = true;
 
         while (!dq.isEmpty()) {
             Node now = dq.poll();
 
+            if (now.x == end.x && now.y == end.y) {
+                return cnt[end.x][end.y];
+            }
+
             for (int i = 0; i < 4; i++) {
                 int nx = now.x;
                 int ny = now.y;
-
-                if (nx + dx[i] < 0 || ny + dy[i] < 0 || nx + dx[i] >= n || ny + dy[i] >= m) {
-                    continue;
-                }
-
-                if (nx + dx[i] == end.x && ny + dy[i] == end.y) {
-                    nx = nx + dx[i] + dx[i];
-                    ny = ny + dy[i] + dy[i];
-
-                    if (board[nx].charAt(ny) == 'D' || nx < 0 || ny < 0 || nx >= n || ny >= m) {
-                        cnt[end.x][end.y] = Math.min(cnt[end.x][end.y], cnt[now.x][now.y] + 1);
-                        return;
-                    }
-                }
 
                 while (true) {
                     nx += dx[i];
                     ny += dy[i];
 
-                    // map 끝에 도달한 경우
-                    if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                    // map 끝에 도달한 경우 or 장애물 만난 경우
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= m || board[nx].charAt(ny) == 'D') {
                         nx -= dx[i];
                         ny -= dy[i];
-
-                        break;
-                    }
-
-                    // 장애물 만난 경우
-                    if (board[nx].charAt(ny) == 'D') {
-                        nx -= dx[i];
-                        ny -= dy[i];
-
                         break;
                     }
                 }
@@ -90,10 +61,12 @@ public class Solution {
                 if (!visited[nx][ny]) {
                     visited[nx][ny] = true;
                     dq.add(new Node(nx, ny));
-                    cnt[nx][ny] = Math.min(cnt[nx][ny], cnt[now.x][now.y] + 1);
+                    cnt[nx][ny] = cnt[now.x][now.y] + 1;
                 }
             }
         }
+
+        return -1;
     }
 
     static public class Node {
