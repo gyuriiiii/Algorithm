01e0afb3bfe3911cbd741c3cfa3159661c37465b
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-07-14 17:51:07 +0900

    [Silver I] Title: 지름길, Time: 224 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static int N, D;
+    static int[] dis;
+    static ArrayList<Node>[] list;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        D = sc.nextInt();
+
+        dis = new int[D + 1];
+        for (int i = 0; i < D + 1; i++) {
+            dis[i] = i;
+        }
+
+        list = new ArrayList[D + 1];
+        for (int i = 0; i < D + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int cost = sc.nextInt();
+
+            if (start > D || end > D)
+                continue;
+
+            list[start].add(new Node(end, cost));
+        }
+
+        dijkstra(0);
+        System.out.println(dis[D]);
+    }
+
+    private static void dijkstra(int start) {
+        if (start >= D) {
+            return;
+        }
+
+        if (dis[start + 1] > dis[start] + 1) {
+            dis[start + 1] = dis[start] + 1;
+        }
+
+        for (Node next : list[start]) {
+            if (dis[next.end] > dis[start] + next.cost) {
+                dis[next.end] = dis[start] + next.cost;
+            }
+        }
+
+        dijkstra(start + 1);
+    }
+
+    static public class Node {
+        int end;
+        int cost;
+
+        public Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+    }
+}
