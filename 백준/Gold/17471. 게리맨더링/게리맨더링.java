0e7e4bc06d02a4be48f203bfd36ca7044fe2f315
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-07-16 16:19:13 +0900

    [Gold IV] Title: 게리맨더링, Time: 228 ms, Memory: 18828 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -3,107 +3,90 @@ import java.util.Scanner;
 
 public class Main {
     static int N;
-    static int[] people;
-    static int[] area;
-    static int[][] map;
-
+    static int[] people, area;
+    static boolean[][] connect;
     static boolean[] visited;
-    static int result;
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt(); // 구역의 개수
+        N = sc.nextInt();
 
-        people = new int[N + 1]; // 구역의 인원수
-        area = new int[N + 1]; // 각 구역의 선거구 저장 (1, 2 선거구)
-        map = new int[N + 1][N + 1]; // 구역 간 인접 표시 배열
+        people = new int[N + 1]; // 각 구역의 인원수
+        area = new int[N + 1]; // 각 구역에 할당된 선거구
+        connect = new boolean[N + 1][N + 1]; // 구역 간 연결 표시 배열
 
-        result = Integer.MAX_VALUE;
-
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i < N + 1; i++) {
             people[i] = sc.nextInt();
         }
 
-        // 각 구역과 인접한 구역 정보
-        for (int i = 1; i <= N; i++) {
-            int num = sc.nextInt();
-            for (int j = 0; j < num; j++) {
+        for (int i = 1; i < N + 1; i++) {
+            int cnt = sc.nextInt();
+            for (int j = 0; j < cnt; j++) {
                 int n = sc.nextInt();
-                // 인접한 구역 1로 표시 (양방향)
-                map[i][n] = 1;
+                connect[i][n] = connect[n][i] = true;
             }
         }
 
-        // 선거구 할당
         dfs(1);
-        if (result == Integer.MAX_VALUE) {
-            System.out.println(-1);
+        if (answer == Integer.MAX_VALUE) {
+            answer = -1;
         }
-        else {
-            System.out.println(result);
-        }
+        System.out.println(answer);
     }
 
-    // 선거구 할당
     private static void dfs(int k) {
-        // 모든 지역 할당한 경우
         if (k == N + 1) {
-            int area1 = 0; // 1번 선거구
-            int area2 = 0; // 2번 선거구
+            int area1 = 0;
+            int area2 = 0;
 
-            // 각 선거구 인원수 구하기
-            for (int i = 1; i <= N; i++) {
-                if (area[i] == 1) {
+            for (int i = 1; i < N + 1; i++) {
+                if (area[i] == 1)
                     area1 += people[i];
-                }
-                else {
+                else if (area[i] == 2)
                     area2 += people[i];
-                }
             }
 
-            visited = new boolean[N + 1]; // 각 구역 방문 여부 배열
+            visited = new boolean[N + 1];
             int link = 0; // 선거구 개수
 
-            // 선거구의 구역들이 모두 연결돼있는지 확인
-            for (int i = 1; i <= N; i++) {
-                if (!visited[i]) { // 아직 방문하지 않은 구역
-                    bfs(i, area[i]);
+            for (int i = 1; i < N + 1; i++) {
+                if (!visited[i]) {
+                    bfs(i);
                     link++;
                 }
             }
 
             if (link == 2) {
-                result = Math.min(result, Math.abs(area1 - area2));
+                answer = Math.min(answer, Math.abs(area1 - area2));
             }
             return;
         }
 
+        // 1번 선거구 할당
         area[k] = 1;
         dfs(k + 1);
 
+        // 2번 선거구 할당
         area[k] = 2;
         dfs(k + 1);
     }
 
-    // 선거구의 구역이 모두 연결돼있는지 확인
-    // 선거구 같고, 구역들이 연결돼있어야 함
-    private static void bfs(int idx, int areaNum) {
+    private static void bfs(int idx) {
         ArrayDeque<Integer> dq = new ArrayDeque<>();
-
-        visited[idx] = true;
         dq.add(idx);
+        visited[idx] = true;
 
         while (!dq.isEmpty()) {
-            int cur = dq.poll();
+            int now = dq.poll();
 
-            for (int i = 1; i <= N; i++) {
-                if (visited[i]) { // 아직 방문 X
+            for (int i = 1; i < N + 1; i++) {
+                if (visited[i])
                     continue;
-                }
 
-                if (map[cur][i] == 1 && area[i] == areaNum) { // 선거구 같고, 서로 연결된 구역인 경우
-                    visited[i] = true; // 방문 표시
+                if (connect[now][i] && area[i] == area[idx]) {
+                    visited[i] = true;
                     dq.add(i);
                 }
             }
