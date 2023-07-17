8978322f04efea3b287a81b21cd380d9b0611eda
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-07-17 15:30:17 +0900

    [unrated] Title: 디펜스 게임, Time: 0.79 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -2,38 +2,35 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Solution {
-    public int solution(int n, int k, int[] enemy) {
-        // PriorityQueue = 기본이 min heap (작은 숫자부터 꺼내짐)
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    static public int solution(int n, int k, int[] enemy) {
+        int result = 0;
 
-        int cnt = 0; // 라운드 수
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); 
 
-        for (int e : enemy) {
-            // 무적권 사용하는  경우
-            // 남은 병사보다 적군 수가 더 많은 경우
-            if (k > 0 && n < e) {
-                pq.add(e); // 큐에 넣기
-                n += pq.poll(); // 가장 큰 수는 빼서 무적권 사용하기 (회복)
-                n -= e; // 들어온 숫자 병사에서 빼기
-                k--; // 무적권 -1
-                cnt++;
+        for (int i = 0; i < enemy.length; i++) {
+            // 무적권 사용하는 경우
+            if (k > 0 && n < enemy[i]) {
+                pq.add(enemy[i]);
+                n += pq.poll(); // 회복
+                n -= enemy[i];
+                k--;
+                result++;
             }
 
-            // 무적권 없는 경우
+            // 무적권 사용하지 않는 경우
             else {
-                // 남아있는 병사가 더 많은 경우
-                if (n >= e) {
-                    pq.add(e);
-                    n -= e;
-                    cnt++;
-                }
-
-                // 남아있는 병사가 더 적은 경우
+                // 남은 병사가 더 많은 경우
+                if (n >= enemy[i]) {
+                    n -= enemy[i];
+                    pq.add(enemy[i]);
+                    result++;
+                } 
+                // 적수가 더 많은 경우 => 종료
                 else {
                     break;
                 }
             }
         }
-        return cnt;
+        return result;
     }
 }
