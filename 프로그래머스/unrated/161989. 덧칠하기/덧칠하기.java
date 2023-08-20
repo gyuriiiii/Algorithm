09e2bece16e86bb01c6ce3a59c68139dd9e76aae
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-08-20 17:29:16 +0900

    [unrated] Title: 덧칠하기, Time: 0.83 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -3,27 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-
-        boolean[] painted = new boolean[n + 1];
-        Arrays.fill(painted, true);
+        int max = 0;
 
         for (int i = 0; i < section.length; i++) {
-            painted[section[i]] = false;
-        }
+            if (section[i] < max) continue;
 
-        for (int i = 0; i < section.length; i++) {
-            if (!painted[section[i]]) {
-                answer++;
-
-                for (int j = section[i]; j < section[i] + m; j++) {
-                    if (j > n) {
-                        break;
-                    }
-                    else {
-                        painted[j] = true;
-                    }
-                }
-            }
+            answer++;
+            max = section[i] + m;
         }
 
         return answer;
