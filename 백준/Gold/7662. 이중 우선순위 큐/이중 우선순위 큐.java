b83e132d9b1e12a3bbe27ce7c8632d85abdc8c17
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-09-15 22:12:34 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 5588 ms, Memory: 449548 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -5,37 +5,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt(); // 입력 데이터 수
+        int T = sc.nextInt();
 
-        for(int i=0; i<T; i++) {
+        for (int j = 0; j < T; j++) {
             TreeMap<Integer, Integer> map = new TreeMap<>();
-            
-            int k = sc.nextInt(); // 큐에 적용할 연산의 개수
 
-            for(int j=0; j<k; j++) {
-                String s = sc.next();
+            int k = sc.nextInt();
+
+            for (int i = 0; i < k; i++) {
+                char order = sc.next().charAt(0);
                 int n = sc.nextInt();
 
-                if(s.equals("I")) { // 삽입
-                    map.put(n, map.getOrDefault(n, 0) + 1); 
+                if (order == 'D') {
+                    if (!map.isEmpty()) {
+                        int num = 0;
+
+                        if (n == 1) {
+                            num = map.lastKey();
+                        }
+                        else {
+                            num = map.firstKey();
+                        }
+
+                        if (map.get(num) > 1) {
+                            map.put(num, map.get(num) - 1);
+                        }
+                        else {
+                            map.remove(num);
+                        }
+                    }
                 }
-                else if(s.equals("D")) { // 삭제
-                    if(map.isEmpty()) {
-                        continue;
-                    }
-
-                    int num = n == -1 ? map.firstKey() : map.lastKey();
-
-                    if(map.get(num) > 1) {
-                        map.put(num, map.get(num)-1);
-                    }
-                    else {
-                        map.remove(num);
-                    }
+                else {
+                    map.put(n, map.getOrDefault(n, 0) + 1);
                 }
             }
 
-            if(map.size() == 0) {
+            if (map.size() == 0) {
                 System.out.println("EMPTY");
             }
             else {
