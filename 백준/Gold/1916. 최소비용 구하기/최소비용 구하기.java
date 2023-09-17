10d9ec8930096716543a1f06e62e0381d393d0d3
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-09-17 17:34:09 +0900

    [Gold V] Title: 최소비용 구하기, Time: 560 ms, Memory: 52568 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,66 +1,69 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static final int INF = 987654321;
+    static ArrayList<Node>[] list;
     static int[] dis;
-    static ArrayList<Node>[] adjList;
     static boolean[] visited;
+    static final int INF = 987654321;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        list = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
 
         dis = new int[N + 1];
         Arrays.fill(dis, INF);
 
-        adjList = new ArrayList[N + 1];
-        for (int i = 0; i < N + 1; i++) {
-            adjList[i] = new ArrayList<>();
-        }
-
         visited = new boolean[N + 1];
 
         for (int i = 0; i < M; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            int cost = sc.nextInt();
-            adjList[s].add(new Node(e, cost));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list[s].add(new Node(e, c));
         }
 
-        int start = sc.nextInt();
-        int end = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
 
         dijkstra(start);
         System.out.println(dis[end]);
     }
 
     private static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>(((o1, o2) -> Integer.compare(o1.cost, o2.cost)));
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
         pq.add(new Node(start, 0));
         dis[start] = 0;
 
         while (!pq.isEmpty()) {
             Node now = pq.poll();
 
-            if (!visited[now.end]) {
-                visited[now.end] = true;
+            if(visited[now.end]) {
+                continue;
+            }
+            visited[now.end] = true;
 
-                for (Node next : adjList[now.end]) {
-                    if (!visited[next.end] && dis[next.end] > now.cost + next.cost) {
-                        dis[next.end] = now.cost + next.cost;
-                        pq.add(new Node(next.end, dis[next.end]));
-                    }
+            for (Node next : list[now.end]) {
+                if (dis[next.end] > now.cost + next.cost) {
+                    dis[next.end] = now.cost + next.cost;
+                    pq.add(new Node(next.end, dis[next.end]));
                 }
             }
         }
     }
 
-    static public class Node {
+    private static class Node {
         int end;
         int cost;
 
