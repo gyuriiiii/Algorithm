54c789007247c734d0d87fdf2482fcb2af7b4906
Gyuri Kim <89003891+gyuriiiii@users.noreply.github.com>
2023-10-24 14:28:29 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1496 ms, Memory: 255368 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
-    static int[] parent;
+    static ArrayList<Node>[] adjList;
+    static boolean[] visited;
+    static int answer;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,53 +13,51 @@ public class Main {
         int V = sc.nextInt();
         int E = sc.nextInt();
 
-        parent = new int[V + 1];
+        adjList = new ArrayList[V + 1];
         for (int i = 1; i < V + 1; i++) {
-            parent[i] = i;
+            adjList[i] = new ArrayList<>();
         }
 
-        ArrayList<Node> list = new ArrayList<>();
+        visited = new boolean[V + 1];
+
         for (int i = 0; i < E; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
             int C = sc.nextInt();
-            list.add(new Node(A, B, C));
+            adjList[A].add(new Node(B, C));
+            adjList[B].add(new Node(A, C));
         }
 
-        list.sort(((o1, o2) -> Integer.compare(o1.cost, o2.cost)));
-
-        int answer = 0;
-        for (Node node : list) {
-            if (find(node.a) != find(node.b)) {
-                union(node.a, node.b);
-                answer += node.cost;
-            }
-        }
-
+        prim(1);
         System.out.println(answer);
     }
 
-    private static void union(int a, int b) {
-        int x = find(a);
-        int y = find(b);
-        parent[x] = y;
-    }
+    private static void prim(int idx) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(((o1, o2) -> Integer.compare(o1.cost, o2.cost)));
+        pq.add(new Node(idx, 0));
 
-    private static int find(int a) {
-        if (parent[a] == a) {
-            return a;
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (!visited[now.end]) {
+                visited[now.end] = true;
+                answer += now.cost;
+
+                for (Node next : adjList[now.end]) {
+                    if (!visited[next.end]) {
+                        pq.add(new Node(next.end, next.cost));
+                    }
+                }
+            }
         }
-        return parent[a] = find(parent[a]);
     }
 
-    static public class Node {
-        int a;
-        int b;
+    private static class Node {
+        int end;
         int cost;
 
-        public Node(int a, int b, int cost) {
-            this.a = a;
-            this.b = b;
+        public Node(int end, int cost) {
+            this.end = end;
             this.cost = cost;
         }
     }
